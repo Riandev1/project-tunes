dca2b8d55e4758fa94a7e2f9a98930a0ae1b02e0
Dev Rian <ocontato.dev@gmail.com>
2023-08-08 19:29:57 -0300

    Feito o requisito 03

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,5 +1,49 @@
-export default function Album() {
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import getMusics from '../services/musicsAPI';
+import { SongType, AlbumType } from '../types';
+import MusicCard from './MusicaCard';
+
+function Album() {
+  const [loading, setLoading] = useState(false);
+  const [musics, setMusics] = useState<SongType[]>([]);
+  const [albuns, setAlbuns] = useState<AlbumType | null>(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getFetch = async () => {
+      setLoading(true);
+      const [albumMusic, ...musicBox] = await getMusics(String(params.id));
+
+      setAlbuns(albumMusic);
+      setMusics(musicBox);
+      setLoading(false);
+    };
+    getFetch();
+  }, [params.id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <h1>Album</h1>
+
+    <>
+      <p>Album</p>
+
+      <h3 data-testid="artist-name">{albuns?.artistName}</h3>
+      <h3 data-testid="album-name">{albuns?.collectionName}</h3>
+      {musics.map((music) => (
+        <MusicCard
+          key={ music.trackName }
+          previewUrl={ music.previewUrl }
+          trackName={ music.trackName }
+          trackId={ music.trackId }
+        />
+      ))}
+    </>
   );
 }
+
+export default Album;
---
+++ src/components/MusicaCard.tsx
@@ -0,0 +1,18 @@
+import { SongType } from '../types';
+
+function MusicCard({ trackName, previewUrl }: SongType) {
+  return (
+    <>
+      <h2>{trackName}</h2>
+      <audio data-testid="audio-component" src={ previewUrl } controls>
+        <track kind="captions" />
+        O seu navegador não suporta o elemento
+        {' '}
+
+        <code>audio</code>
+        .
+      </audio>
+    </>
+  );
+}
+export default MusicCard;
